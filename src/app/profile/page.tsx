058ac4910b2f12e9c88553e18ab028cfe3fdf13c
sivakumar <b121933rgukt@gmail.com>
2025-01-31 10:39:02 +0530

    fix done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,67 +1,12 @@
 "use client";
-import { AppContext } from "@/app/Context";
-import useAxios from "@/helpers/useAxios";
 import CopyIcon from "@/svg/CopyIcon";
-import { useAccount } from "@particle-network/connectkit";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import React, { useContext, useEffect, useState } from "react";
-import LeftPanel from "../../components/leftPanel";
 import arrowRight from "../../assets/arrowRight.svg";
 import blayLogo from "../../assets/blayLogo.png";
 import userAvatar from "../../assets/user-avatar.webp";
+import LeftPanel from "../../components/leftPanel";
 
 export default function Profile() {
-  const navigate = useRouter();
-  const { onCall: createUser, loading } = useAxios({
-    api: "/user/create",
-    method: "post",
-    needToken: true,
-  });
-  const { onCall: sendMessage, loading: msgLoading } = useAxios({
-    api: "/user/message",
-    method: "post",
-    needToken: true,
-  });
-  const { onCall: getMessages } = useAxios({
-    api: "/user/messages",
-    method: "get",
-    needToken: true,
-  });
-  const [msg, setMsg] = useState("");
-  const [resMsgs, setResMsg] = useState<any>([]);
-  const { showChat, setShowChat } = useContext(AppContext);
-  const { isDisconnected, isConnecting, isReconnecting } = useAccount();
-  const handleToggle = () => {
-    createUser({}).then((res) => {
-      if (res?.result) setShowChat(true); // Toggle the state
-    });
-  };
-
-  const bottomRef = React.useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (bottomRef.current) {
-      bottomRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [resMsgs]);
-
-  useEffect(() => {
-    if (!isConnecting && !isReconnecting && isDisconnected) {
-      window.sessionStorage.clear();
-      navigate.push("/");
-    }
-  }, [isDisconnected, isConnecting, isReconnecting]);
-
-  useEffect(() => {
-    if (showChat) {
-      getMessages({}).then((res) => {
-        console.log(res?.result);
-
-        if (res?.result?.length) setResMsg(res?.result);
-      });
-    }
-  }, [showChat]);
   return (
     <div className="blay-main">
       <div className="header">
