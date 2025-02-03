118796fa65292d6b5df99c179e9050f7c2d2826e
sivakumar <b121933rgukt@gmail.com>
2025-02-03 11:18:34 +0530

    handle copy added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,8 @@ import { formattedAddress } from "@/helpers/utils";
 import CopyIcon from "@/svg/CopyIcon";
 import { useAccount, useDisconnect } from "@particle-network/connectkit";
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import arrowRight from "../../assets/arrowRight.svg";
-import blayLogo from "../../assets/blayLogo.png";
 import userAvatar from "../../assets/user-avatar.webp";
 import LeftPanel from "../../components/leftPanel";
 import { AppContext } from "../Context";
@@ -14,11 +13,39 @@ import lineDot from "../../assets/line.svg";
 export default function Profile() {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
-  const { user } = useContext(AppContext);
+  const { user, setNavLoading } = useContext(AppContext);
+  const [tooltipVisible, setTooltipVisible] = useState<any>({});
+
+  useEffect(() => {
+    setNavLoading(false)
+  }, [])
+
+  const copyToClipboard = (text: string, key: string) => {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        console.log("Text copied to clipboard");
+        showTooltip(key); // Show tooltip after copying
+      },
+      (err) => {
+        console.error("Failed to copy text: ", err);
+      }
+    );
+  };
+
+  const showTooltip = (key: string) => {
+    setTooltipVisible({ [key]: true });
+
+    // Hide the tooltip after 2 seconds
+    const to = setTimeout(() => {
+      setTooltipVisible({ [key]: false });
+
+      clearTimeout(to);
+    }, 2000);
+  };
 
   return (
     <div className="blay-main">
- 
+
       <div className="container flex">
         <div className="item1">
           <LeftPanel />
@@ -59,7 +86,10 @@ export default function Profile() {
                               <div>{formattedAddress(address)}</div>
                             </div>
                           </div>
-                          <div className="copy-wallet">
+                          <div className="copy-wallet" onClick={() => { if (address) copyToClipboard(address, "addr") }}>
+                            {tooltipVisible?.addr && (
+                              <div className="copy-text">Copied!</div>
+                            )}
                             <CopyIcon />
                           </div>
                         </div>
@@ -69,10 +99,13 @@ export default function Profile() {
                             <div className="p-block">P</div>
                             <div className="p-wallet">
                               <div>My Blay Wallet</div>
-                              <div>{formattedAddress(address)}</div>
+                              <div>{formattedAddress(user?.wallet_address)}</div>
                             </div>
                           </div>
-                          <div className="copy-wallet">
+                          <div className="copy-wallet" onClick={() => { if (user?.wallet_address) copyToClipboard(user?.wallet_address, "waddr") }}>
+                            {tooltipVisible?.waddr && (
+                              <div className="copy-text">Copied!</div>
+                            )}
                             <CopyIcon />
                           </div>
                         </div>
--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -7,19 +7,21 @@ import {
 import profilePic from "../assets/profile-pic.png";
 
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 import { formattedAddress } from "@/helpers/utils";
 import { useRouter } from "next/navigation";
 import { config } from "./providers";
+import { AppContext } from "@/app/Context";
 
 const CustomButton = () => {
   const { isConnected, address, chainId } = useAccount();
-  const { connect } = useConnect();
+  const { setNavLoading } = useContext(AppContext)
   const chains = useChains();
   const navigate = useRouter();
   return isConnected ? (
     <div
       onClick={() => {
+        setNavLoading(true)
         navigate.push("/profile");
       }}
       className="profile-address"
